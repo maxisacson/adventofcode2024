30681ad69397bfd143c32347bd7618277a332d92
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2024-12-07 10:05:49 +0100

    day5: wip2 zig solution

--- day5.zig
+++ day5.zig
@@ -68,6 +68,45 @@ fn checkLine(line: []const u8, rules: *const std.StringHashMap(std.ArrayList([]c
     return numbers.items[@divTrunc(numbers.items.len, 2)];
 }
 
+fn fixLine(line: []const u8, rules: *const std.StringHashMap(std.ArrayList([]const u8))) !i32 {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    var indices = std.StringHashMap(usize).init(alloc);
+    defer indices.deinit();
+
+    var forbidden = std.StringHashMap(void).init(alloc);
+    defer forbidden.deinit();
+
+    var numbers = std.ArrayList(i32).init(alloc);
+    defer numbers.deinit();
+
+    var bad = std.ArrayList(bool).init(alloc);
+    defer bad.deinit();
+
+    var iter = std.mem.tokenize(u8, line, ",");
+    while (iter.next()) |n| {
+        const x = try std.fmt.parseInt(i32, n, 10);
+        try numbers.append(x);
+        try bad.append(false);
+    }
+
+    iter.reset();
+    while (iter.next()) |n| {
+        if (forbidden.contains(n)) {
+            return 0;
+        }
+
+        if (rules.*.contains(n)) {
+            const items = rules.*.getPtr(n).?.*.items;
+            for (items) |item| {
+                _ = try forbidden.getOrPutValue(item, {});
+            }
+        }
+    }
+}
+
 fn run(input: []const u8) !i32 {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -117,6 +156,53 @@ fn run(input: []const u8) !i32 {
 }
 
 fn run2(input: []const u8) !u32 {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    var row = std.mem.split(u8, input, "\n");
+
+    // parse rules
+    var rules = std.StringHashMap(std.ArrayList([]const u8)).init(alloc);
+
+    while (row.next()) |line| {
+        if (line.len == 0) {
+            break;
+        }
+        var col = std.mem.tokenize(u8, line, "|");
+        const a = col.next().?;
+        const b = col.next().?;
+        if (rules.contains(b)) {
+            const list = rules.getPtr(b).?;
+            try list.*.append(a);
+        } else {
+            var list = std.ArrayList([]const u8).init(alloc);
+            try list.append(a);
+            try rules.put(b, list);
+        }
+    }
+
+    // var iter = rules.iterator();
+    // while (iter.next()) |rule| {
+    //     print("{s}: ", .{rule.key_ptr.*});
+    //     for (rule.value_ptr.*.items) |item| {
+    //         print(" {s}", .{item});
+    //     }
+    //     print("\n", .{});
+    // }
+
+    // parse updates
+    var sum: i32 = 0;
+    while (row.next()) |line| {
+        if (line.len == 0) {
+            break;
+        }
+        if (try !checkLine(line, &rules)) {
+            sum += try fixLine(line, &rules);
+        }
+    }
+
+    // return sum;
     return @intCast(input.len);
 }
 
@@ -129,4 +215,8 @@ pub fn main() !void {
 test "day5" {
     try testing.expectEqual(143, try run(test_input));
     // try testing.expectEqual(123, try run2(test_input));
+
+    const text = try readFile("day5.txt");
+    try testing.expectEqual(4662, try run(text));
+    try testing.expectEqual(5900, try run2(text));
 }
